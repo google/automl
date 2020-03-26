ff5b11d91f399c4b9e8b35d4e548d13c0f26effa
Mingxing Tan <tanmingxing@google.com>
2020-03-25 22:20:07 -0700

    Remove unneeded dependence and code.
    This is pointed out by @CraigWang1 in https://github.com/google/automl/pull/52/files

--- efficientdet/dataset/label_map_util.py
+++ efficientdet/dataset/label_map_util.py
@@ -19,13 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import logging
-from six import string_types
 from six.moves import range
-
-import tensorflow.compat.v1 as tf
-from google.protobuf import text_format
-
-from dataset import string_int_label_map_pb2
 
 
 def _validate_label_map(label_map):
@@ -139,125 +133,6 @@ def convert_label_map_to_categories(label_map,
         category['keypoints'] = keypoints
       categories.append(category)
   return categories
-
-
-def load_labelmap(path):
-  """Loads label map proto.
-
-  Args:
-    path: path to StringIntLabelMap proto text file.
-  Returns:
-    a StringIntLabelMapProto
-  """
-  with tf.io.gfile.GFile(path, 'r') as fid:
-    label_map_string = fid.read()
-    label_map = string_int_label_map_pb2.StringIntLabelMap()
-    try:
-      text_format.Merge(label_map_string, label_map)
-    except text_format.ParseError:
-      label_map.ParseFromString(label_map_string)
-  _validate_label_map(label_map)
-  return label_map
-
-
-def get_label_map_dict(label_map_path_or_proto,
-                       use_display_name=False,
-                       fill_in_gaps_and_background=False):
-  """Reads a label map and returns a dictionary of label names to id.
-
-  Args:
-    label_map_path_or_proto: path to StringIntLabelMap proto text file or the
-      proto itself.
-    use_display_name: whether to use the label map items' display names as keys.
-    fill_in_gaps_and_background: whether to fill in gaps and background with
-    respect to the id field in the proto. The id: 0 is reserved for the
-    'background' class and will be added if it is missing. All other missing
-    ids in range(1, max(id)) will be added with a dummy class name
-    ("class_<id>") if they are missing.
-
-  Returns:
-    A dictionary mapping label names to id.
-
-  Raises:
-    ValueError: if fill_in_gaps_and_background and label_map has non-integer or
-    negative values.
-  """
-  if isinstance(label_map_path_or_proto, string_types):
-    label_map = load_labelmap(label_map_path_or_proto)
-  else:
-    _validate_label_map(label_map_path_or_proto)
-    label_map = label_map_path_or_proto
-
-  label_map_dict = {}
-  for item in label_map.item:
-    if use_display_name:
-      label_map_dict[item.display_name] = item.id
-    else:
-      label_map_dict[item.name] = item.id
-
-  if fill_in_gaps_and_background:
-    values = set(label_map_dict.values())
-
-    if 0 not in values:
-      label_map_dict['background'] = 0
-    if not all(isinstance(value, int) for value in values):
-      raise ValueError('The values in label map must be integers in order to'
-                       'fill_in_gaps_and_background.')
-    if not all(value >= 0 for value in values):
-      raise ValueError('The values in the label map must be positive.')
-
-    if len(values) != max(values) + 1:
-      # there are gaps in the labels, fill in gaps.
-      for value in range(1, max(values)):
-        if value not in values:
-          # TODO(rathodv): Add a prefix 'class_' here once the tool to generate
-          # teacher annotation adds this prefix in the data.
-          label_map_dict[str(value)] = value
-
-  return label_map_dict
-
-
-def create_categories_from_labelmap(label_map_path, use_display_name=True):
-  """Reads a label map and returns categories list compatible with eval.
-
-  This function converts label map proto and returns a list of dicts, each of
-  which  has the following keys:
-    'id': an integer id uniquely identifying this category.
-    'name': string representing category name e.g., 'cat', 'dog'.
-    'keypoints': a dictionary of keypoint string label to integer id. It is only
-      returned when available in label map proto.
-
-  Args:
-    label_map_path: Path to `StringIntLabelMap` proto text file.
-    use_display_name: (boolean) choose whether to load 'display_name' field
-      as category name.  If False or if the display_name field does not exist,
-      uses 'name' field as category names instead.
-
-  Returns:
-    categories: a list of dictionaries representing all possible categories.
-  """
-  label_map = load_labelmap(label_map_path)
-  max_num_classes = max(item.id for item in label_map.item)
-  return convert_label_map_to_categories(label_map, max_num_classes,
-                                         use_display_name)
-
-
-def create_category_index_from_labelmap(label_map_path, use_display_name=True):
-  """Reads a label map and returns a category index.
-
-  Args:
-    label_map_path: Path to `StringIntLabelMap` proto text file.
-    use_display_name: (boolean) choose whether to load 'display_name' field
-      as category name.  If False or if the display_name field does not exist,
-      uses 'name' field as category names instead.
-
-  Returns:
-    A category index, which is a dictionary that maps integer ids to dicts
-    containing categories, e.g.
-    {1: {'id': 1, 'name': 'dog'}, 2: {'id': 2, 'name': 'cat'}, ...}
-  """
-  categories = create_categories_from_labelmap(label_map_path, use_display_name)
-  return create_category_index(categories)
 
 
 def create_class_agnostic_category_index():
