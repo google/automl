53286d6f2d4ab44999df9e968a7712c5117c4a6c
Mingxing Tan <tanmingxing@google.com>
2020-04-05 12:21:05 -0700

    Replace tf.collection to avoid unnecessary warning.
    A small issue raise in https://github.com/google/automl/issues/29

--- efficientdet/utils.py
+++ efficientdet/utils.py
@@ -314,18 +314,19 @@ def num_params_flops(readable_format=True):
 
 conv_kernel_initializer = tf.initializers.variance_scaling()
 dense_kernel_initializer = tf.initializers.variance_scaling()
-
+summaries = []
 
 def scalar(name, tensor):
   """Stores a (name, Tensor) tuple in a custom collection."""
   logging.info('Adding summary {}'.format((name, tensor)))
-  tf.add_to_collection('edsummaries', (name, tf.reduce_mean(tensor)))
+  global summaries
+  summaries.append((name, tf.reduce_mean(tensor)))
 
 
 def get_scalar_summaries():
   """Returns the list of (name, Tensor) summaries recorded by scalar()."""
-  logging.info('get summaries {}'.format(tf.get_collection('edsummaries')))
-  return tf.get_collection('edsummaries')
+  global summaries
+  return summaries
 
 
 def get_tpu_host_call(global_step, params):
