0f09929f6e745c104e9f6493fb1752a2174244e9
fsx950223 <fsx950223@gmail.com>
2020-05-08 17:48:57 +0800

    add graph test

--- efficientdet/keras_impl/efficientdet_arch_keras_test.py
+++ efficientdet/keras_impl/efficientdet_arch_keras_test.py
@@ -1,6 +1,7 @@
 import tensorflow.compat.v1 as tf
 import efficientdet_arch
 from keras_impl import efficientdet_arch_keras as efficientdet_arch_keras
+from tensorflow.python.framework.test_util import deprecated_graph_mode_only
 
 class KerasTest(tf.test.TestCase):
   def test_resample_feature_map(self):
@@ -8,7 +9,7 @@ class KerasTest(tf.test.TestCase):
     for apply_fn in [True, False]:
       for is_training in [True, False]:
         for use_tpu in [True, False]:
-          with self.subTest(apply_fn=apply_fn,is_training=is_training, use_tpu=use_tpu):
+          with self.subTest(apply_fn=apply_fn, is_training=is_training, use_tpu=use_tpu):
             tf.random.set_random_seed(111111)
             expect_result = efficientdet_arch.resample_feature_map(feat,
                                                                    name='resample_p0',
@@ -28,8 +29,8 @@ class KerasTest(tf.test.TestCase):
                                                                        use_tpu=use_tpu)(feat)
             self.assertAllCloseAccordingToType(expect_result, actual_result)
 
+  @deprecated_graph_mode_only
   def test_name(self):
-    tf.disable_eager_execution()
     feat = tf.random.uniform([1, 16, 16, 320])
     actual_result = efficientdet_arch_keras.ResampleFeatureMap(name='p0',
                                                                target_height=8,
