ab4bcaba3cddc635050ee0ed676b77913107c0e4
Jon <jonbakerfish@gmail.com>
2020-05-22 15:08:57 +0800

    fix inference_time calculation bug

--- efficientdet/model_inspect.py
+++ efficientdet/model_inspect.py
@@ -381,7 +381,7 @@ class ModelInspector(object):
       for i in range(bm_runs):
         sess.run(output_name, feed_dict={input_name: img})
       end = time.perf_counter()
-      inference_time = (end - start) / 10
+      inference_time = (end - start) / bm_runs
       print('Per batch inference time: ', inference_time)
       print('FPS: ', self.batch_size / inference_time)
 
