533faa9058d3894db44d45acce415d75c2749bfc
fsx950223 <fsx950223@gmail.com>
2020-06-08 16:42:56 +0800

    change to input

--- efficientdet/main.py
+++ efficientdet/main.py
@@ -242,12 +242,9 @@ def main(_):
       .PER_HOST_V2)
 
   if FLAGS.model_dir is None:
-    import time
-    #get time stamp
-    now = int(time.time())
-    time_arr = time.localtime(now)
-    time_stamp = time.strftime("%Y-%m-%d-%H-%M-%S", time_arr)
-    model_dir = os.path.join(FLAGS.model_name, time_stamp)
+    model_dir = input("Please specific model dir: ")
+  else:
+    model_dir = FLAGS.model_dir
   
   if FLAGS.strategy == 'horovod':
     model_dir = model_dir if hvd.rank() == 0 else None
