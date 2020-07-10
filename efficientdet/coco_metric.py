9562e553163ac1732a29f2e3702a586121181d65
Mingxing Tan <tanmingxing@google.com>
2020-07-09 18:35:18 -0700

    Add keras model eval script.

--- efficientdet/coco_metric.py
+++ efficientdet/coco_metric.py
@@ -38,10 +38,13 @@ import tensorflow.compat.v1 as tf
 FLAGS = flags.FLAGS
 
 
-class EvaluationMetric(object):
-  """COCO evaluation metric class."""
+class EvaluationMetric():
+  """COCO evaluation metric class.
 
-  def __init__(self, filename=None, testdev_dir=None):
+  This class cannot inherit from tf.keras.metrics.Metric due to numpy.
+  """
+
+  def __init__(self, filename=None, testdev_dir=None, **kwargs):
     """Constructs COCO evaluation class.
 
     The class provides the interface to metrics_fn in TPUEstimator. The
@@ -56,18 +59,14 @@ class EvaluationMetric(object):
       testdev_dir: folder name for testdev data. If None, run eval without
         groundtruth, and filename will be ignored.
     """
-    if filename:
-      self.coco_gt = COCO(filename)
     self.filename = filename
     self.testdev_dir = testdev_dir
     self.metric_names = ['AP', 'AP50', 'AP75', 'APs', 'APm', 'APl', 'ARmax1',
                          'ARmax10', 'ARmax100', 'ARs', 'ARm', 'ARl']
-    self._reset()
+    self.reset_states()
 
-  def _reset(self):
+  def reset_states(self):
     """Reset COCO API object."""
-    if self.filename is None:
-      self.coco_gt = COCO()
     self.detections = []
     self.dataset = {
         'images': [],
@@ -77,6 +76,124 @@ class EvaluationMetric(object):
     self.image_id = 1
     self.annotation_id = 1
     self.category_ids = []
+    self.metric_values = None
+
+  def evaluate(self):
+    """Evaluates with detections from all images with COCO API.
+
+    Returns:
+      coco_metric: float numpy array with shape [12] representing the
+        coco-style evaluation metrics.
+    """
+    if self.filename:
+      self.coco_gt = COCO(self.filename)
+    else:
+      self.coco_gt.dataset = self.dataset
+      self.coco_gt.createIndex()
+
+    if self.testdev_dir:
+      # Run on test-dev dataset.
+      box_result_list = []
+      for det in self.detections:
+        box_result_list.append({
+            'image_id': int(det[0]),
+            'category_id': int(det[6]),
+            'bbox': np.around(
+                det[1:5].astype(np.float64), decimals=2).tolist(),
+            'score': float(np.around(det[5], decimals=3)),
+        })
+      json.encoder.FLOAT_REPR = lambda o: format(o, '.3f')
+      # Must be in the formst of 'detections_test-dev2017_xxx_results'.
+      fname = 'detections_test-dev2017_test_results'
+      output_path = os.path.join(self.testdev_dir, fname + '.json')
+      logging.info('Writing output json file to: %s', output_path)
+      with tf.io.gfile.GFile(output_path, 'w') as fid:
+        json.dump(box_result_list, fid)
+      zip_path = os.path.join(self.testdev_dir, fname + '.zip')
+      with zipfile.ZipFile(zip_path, 'w') as zf:
+        zf.writestr(fname + '.json', json.dumps(box_result_list))
+      return np.array([0.], dtype=np.float32)
+    else:
+      # Run on validation dataset.
+      detections = np.array(self.detections)
+      image_ids = list(set(detections[:, 0]))
+      coco_dt = self.coco_gt.loadRes(detections)
+      coco_eval = COCOeval(self.coco_gt, coco_dt, iouType='bbox')
+      coco_eval.params.imgIds = image_ids
+      coco_eval.evaluate()
+      coco_eval.accumulate()
+      coco_eval.summarize()
+      coco_metrics = coco_eval.stats
+      return np.array(coco_metrics, dtype=np.float32)
+
+  def result(self):
+    """Return the metric values (and compute it if needed)."""
+    if not self.metric_values:
+      self.metric_values = self.evaluate()
+    return self.metric_values
+
+  def update_state(self, groundtruth_data, detections):
+    """Update detection results and groundtruth data.
+
+    Append detection results to self.detections to aggregate results from
+    all validation set. The groundtruth_data is parsed and added into a
+    dictionary with the same format as COCO dataset, which can be used for
+    evaluation.
+
+    Args:
+      detections: Detection results in a tensor with each row representing
+        [image_id, x, y, width, height, score, class].
+      groundtruth_data: Groundtruth annotations in a tensor with each row
+        representing [y1, x1, y2, x2, is_crowd, area, class].
+    """
+    for i in range(len(detections)):
+      # Filter out detections with predicted class label = -1.
+
+      indices = np.where(detections[i, :, -1] > -1)[0]
+      detections[i] = detections[i, indices]
+      if detections[i].shape[0] == 0:
+        continue
+      # Append groundtruth annotations to create COCO dataset object.
+      # Add images.
+      image_id = detections[i][0, 0]
+      if image_id == -1:
+        image_id = self.image_id
+      detections[i][:, 0] = image_id
+      self.detections.extend(detections[i])
+
+      if self.testdev_dir:
+        # Skip annotation for test-dev case.
+        self.image_id += 1
+        continue
+
+      self.dataset['images'].append({
+          'id': int(image_id),
+      })
+
+      # Add annotations.
+      indices = np.where(groundtruth_data[i, :, -1] > -1)[0]
+      for data in groundtruth_data[i, indices]:
+        box = data[0:4]
+        is_crowd = data[4]
+        area = (box[3] - box[1]) * (box[2] - box[0])
+        category_id = data[6]
+        if category_id < 0:
+          break
+        self.dataset['annotations'].append({
+            'id': int(self.annotation_id),
+            'image_id': int(image_id),
+            'category_id': int(category_id),
+            'bbox': [box[1], box[0], box[3] - box[1], box[2] - box[0]],
+            'area': area,
+            'iscrowd': int(is_crowd)
+        })
+        self.annotation_id += 1
+        self.category_ids.append(category_id)
+      self.image_id += 1
+    self.category_ids = list(set(self.category_ids))
+    self.dataset['categories'] = [
+        {'id': int(category_id)} for category_id in self.category_ids
+    ]
 
   def estimator_metric_fn(self, detections, groundtruth_data):
     """Constructs the metric function for tf.TPUEstimator.
@@ -96,130 +213,17 @@ class EvaluationMetric(object):
         operations (`metric_op`, `update_op`). `update_op` appends the
         detections for the metric to the `self.detections` list.
     """
-
-    def _evaluate():
-      """Evaluates with detections from all images with COCO API.
-
-      Returns:
-        coco_metric: float numpy array with shape [12] representing the
-          coco-style evaluation metrics.
-      """
-      if self.filename is None:
-        self.coco_gt.dataset = self.dataset
-        self.coco_gt.createIndex()
-
-      if self.testdev_dir:
-        # Run on test-dev dataset.
-        box_result_list = []
-        for det in self.detections:
-          box_result_list.append({
-              'image_id': int(det[0]),
-              'category_id': int(det[6]),
-              'bbox': np.around(
-                  det[1:5].astype(np.float64), decimals=2).tolist(),
-              'score': float(np.around(det[5], decimals=3)),
-          })
-        json.encoder.FLOAT_REPR = lambda o: format(o, '.3f')
-        # Must be in the formst of 'detections_test-dev2017_xxx_results'.
-        fname = 'detections_test-dev2017_test_results'
-        output_path = os.path.join(self.testdev_dir, fname + '.json')
-        logging.info('Writing output json file to: %s', output_path)
-        with tf.io.gfile.GFile(output_path, 'w') as fid:
-          json.dump(box_result_list, fid)
-        zip_path = os.path.join(self.testdev_dir, fname + '.zip')
-        with zipfile.ZipFile(zip_path, 'w') as zf:
-          zf.writestr(fname + '.json', json.dumps(box_result_list))
-
-        self._reset()
-        return np.array([0.], dtype=np.float32)
-      else:
-        # Run on validation dataset.
-        detections = np.array(self.detections)
-        image_ids = list(set(detections[:, 0]))
-        coco_dt = self.coco_gt.loadRes(detections)
-        coco_eval = COCOeval(self.coco_gt, coco_dt, iouType='bbox')
-        coco_eval.params.imgIds = image_ids
-        coco_eval.evaluate()
-        coco_eval.accumulate()
-        coco_eval.summarize()
-        coco_metrics = coco_eval.stats
-        # clean self.detections after evaluation is done.
-        # this makes sure the next evaluation will start with an empty list of
-        # self.detections.
-        self._reset()
-        return np.array(coco_metrics, dtype=np.float32)
-
-    def _update_op(detections, groundtruth_data):
-      """Update detection results and groundtruth data.
-
-      Append detection results to self.detections to aggregate results from
-      all validation set. The groundtruth_data is parsed and added into a
-      dictionary with the same format as COCO dataset, which can be used for
-      evaluation.
-
-      Args:
-       detections: Detection results in a tensor with each row representing
-         [image_id, x, y, width, height, score, class].
-       groundtruth_data: Groundtruth annotations in a tensor with each row
-         representing [y1, x1, y2, x2, is_crowd, area, class].
-      """
-      for i in range(len(detections)):
-        # Filter out detections with predicted class label = -1.
-        indices = np.where(detections[i, :, -1] > -1)[0]
-        detections[i] = detections[i, indices]
-        if detections[i].shape[0] == 0:
-          continue
-        # Append groundtruth annotations to create COCO dataset object.
-        # Add images.
-        image_id = detections[i][0, 0]
-        if image_id == -1:
-          image_id = self.image_id
-        detections[i][:, 0] = image_id
-        self.detections.extend(detections[i])
-
-        if self.testdev_dir:
-          # Skip annotation for test-dev case.
-          self.image_id += 1
-          continue
-
-        self.dataset['images'].append({
-            'id': int(image_id),
-        })
-
-        # Add annotations.
-        indices = np.where(groundtruth_data[i, :, -1] > -1)[0]
-        for data in groundtruth_data[i, indices]:
-          box = data[0:4]
-          is_crowd = data[4]
-          area = (box[3] - box[1]) * (box[2] - box[0])
-          category_id = data[6]
-          if category_id < 0:
-            break
-          self.dataset['annotations'].append({
-              'id': int(self.annotation_id),
-              'image_id': int(image_id),
-              'category_id': int(category_id),
-              'bbox': [box[1], box[0], box[3] - box[1], box[2] - box[0]],
-              'area': area,
-              'iscrowd': int(is_crowd)
-          })
-          self.annotation_id += 1
-          self.category_ids.append(category_id)
-        self.image_id += 1
-      self.category_ids = list(set(self.category_ids))
-      self.dataset['categories'] = [
-          {'id': int(category_id)} for category_id in self.category_ids
-      ]
-
     with tf.name_scope('coco_metric'):
       if self.testdev_dir:
-        update_op = tf.py_func(_update_op, [detections, groundtruth_data], [])
-        metrics = tf.py_func(_evaluate, [], tf.float32)
+        update_op = tf.py_func(self.update_state,
+                               [groundtruth_data, detections], [])
+        metrics = tf.py_func(self.result, [], tf.float32)
         metrics_dict = {'AP': (metrics, update_op)}
         return metrics_dict
       else:
-        update_op = tf.py_func(_update_op, [detections, groundtruth_data], [])
-        metrics = tf.py_func(_evaluate, [], tf.float32)
+        update_op = tf.py_func(self.update_state,
+                               [groundtruth_data, detections], [])
+        metrics = tf.py_func(self.result, [], tf.float32)
         metrics_dict = {}
         for i, name in enumerate(self.metric_names):
           metrics_dict[name] = (metrics[i], update_op)
