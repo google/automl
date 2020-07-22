90029a2d659ca11a9ea4475ad968fff52f337017
fsx950223 <fsx950223@gmail.com>
2020-07-22 10:06:24 +0800

    fix flags bug

--- efficientdet/keras/eval.py
+++ efficientdet/keras/eval.py
@@ -83,6 +83,6 @@ def main(_):
 if __name__ == '__main__':
   flags.mark_flag_as_required('val_file_pattern')
   flags.mark_flag_as_required('val_json_file')
-  flags.mark_flag_as_required('checkpoint')
+  flags.mark_flag_as_required('model_dir')
   logging.set_verbosity(logging.WARNING)
   app.run(main)
