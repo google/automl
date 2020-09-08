009d6fea0e0fe0a59666069c1b6de1b33b77539e
sadra <sadra.nsh@gmail.com>
2020-09-07 22:23:34 -0500

    added save_checkpoints_steps as flag (#747)
    * added save_checkpoints_steps as flag
    
    * added save_checkpoints_steps to TPUConfig

--- efficientdet/main.py
+++ efficientdet/main.py
@@ -79,6 +79,8 @@ flags.DEFINE_integer('eval_samples', 5000, 'The number of samples for '
                      'evaluation.')
 flags.DEFINE_integer('iterations_per_loop', 100,
                      'Number of iterations per TPU training loop')
+flags.DEFINE_integer('save_checkpoints_steps', 5000,
+                     'Number of iterations per checkpoint save')
 flags.DEFINE_string(
     'training_file_pattern', None,
     'Glob for training data files (e.g., COCO train - minival set)')
@@ -218,6 +220,7 @@ def main(_):
       FLAGS.iterations_per_loop if FLAGS.strategy == 'tpu' else 1,
       num_cores_per_replica=num_cores_per_replica,
       input_partition_dims=input_partition_dims,
+      save_checkpoints_steps=FLAGS.save_checkpoints_steps,
       per_host_input_for_training=tf.estimator.tpu.InputPipelineConfig
       .PER_HOST_V2)
 
@@ -240,7 +243,7 @@ def main(_):
         train_distribute=strategy,
         log_step_count_steps=FLAGS.iterations_per_loop,
         session_config=config_proto,
-        save_checkpoints_steps=5000,
+        save_checkpoints_steps=FLAGS.save_checkpoints_steps,
         tf_random_seed=FLAGS.tf_random_seed,
     )
 
