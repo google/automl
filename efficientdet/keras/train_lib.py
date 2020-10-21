099c5d05219f40660dab94cf403a5e6598937f19
fsx950223 <fsx950223@gmail.com>
2020-10-21 18:50:57 +0800

    add comment

--- efficientdet/keras/train_lib.py
+++ efficientdet/keras/train_lib.py
@@ -31,8 +31,56 @@ from tensorflow_model_optimization.python.core.sparsity.keras import pruning_wra
 
 
 class MomentumOptimizer(tf.keras.optimizers.Optimizer):
+  """Optimizer that implements the Momentum algorithm.
+
+  Computes (if `use_nesterov = False`):
+
+  ```
+  accumulation = momentum * accumulation + gradient
+  variable -= learning_rate * accumulation
+  ```
+
+  Note that in the dense version of this algorithm, `accumulation` is updated
+  and applied regardless of a gradient's value, whereas the sparse version (when
+  the gradient is an `IndexedSlices`, typically because of `tf.gather` or an
+  embedding) only updates variable slices and corresponding `accumulation` terms
+  when that part of the variable was used in the forward pass.
+
+  A tf1.x MomentumOptimizer for tf2.x
+  """
   def __init__(self, learning_rate, momentum,
                use_nesterov=False, name='Momentum', **kwargs):
+    """Construct a new Momentum optimizer.
+
+    Args:
+      learning_rate: A `Tensor` or a floating point value.  The learning rate.
+      momentum: A `Tensor` or a floating point value.  The momentum.
+      use_nesterov: If `True` use Nesterov Momentum.
+        See (Sutskever et al., 2013).
+        This implementation always computes gradients at the value of the
+        variable(s) passed to the optimizer. Using Nesterov Momentum makes the
+        variable(s) track the values called `theta_t + mu*v_t` in the paper.
+        This implementation is an approximation of the original formula, valid
+        for high values of momentum. It will compute the "adjusted gradient"
+        in NAG by assuming that the new gradient will be estimated by the
+        current average gradient plus the product of momentum and the change
+        in the average gradient.
+      name: Optional name prefix for the operations created when applying
+        gradients.  Defaults to "Momentum".
+
+    References:
+      On the importance of initialization and momentum in deep learning:
+        [Sutskever et al., 2013]
+        (http://proceedings.mlr.press/v28/sutskever13.html)
+        ([pdf](http://proceedings.mlr.press/v28/sutskever13.pdf))
+
+    @compatibility(eager)
+    When eager execution is enabled, `learning_rate` and `momentum` can each be
+    a callable that takes no arguments and returns the actual value to use. This
+    can be useful for changing these values across different invocations of
+    optimizer functions.
+    @end_compatibility
+    """
     super().__init__(name=name, **kwargs)
     self._set_hyper('learning_rate', learning_rate)
     self._set_hyper('decay', self._initial_decay)
