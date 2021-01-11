fix mixed precision eval when use COCOCallback

efficientdet/keras/postprocess.py
@@ -28,9 +28,9 @@ CLASS_OFFSET = 1
 
 def to_list(inputs):
   if isinstance(inputs, dict):
-    return [inputs[k] for k in sorted(inputs.keys())]
+    return [tf.cast(inputs[k], tf.float32) for k in sorted(inputs.keys())]
   if isinstance(inputs, list):
-    return inputs
+    return [tf.cast(i, tf.float32) for i in inputs]
   raise ValueError('Unrecognized inputs : {}'.format(inputs))
 
 