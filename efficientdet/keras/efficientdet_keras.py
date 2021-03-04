c4570520b9b2c069e326ffb7b2fbbcd1f4dde679
fsx950223 <fsx950223@gmail.com>
2021-03-04 12:39:43 +0800

    fix fastattn

--- efficientdet/keras/efficientdet_keras.py
+++ efficientdet/keras/efficientdet_keras.py
@@ -87,18 +87,14 @@ class FNode(tf.keras.layers.Layer):
     dtype = nodes[0].dtype
 
     if self.weight_method == 'attn':
-      edge_weights = []
-      for var in self.vars:
-        var = tf.cast(var, dtype=dtype)
-        edge_weights.append(var)
+      edge_weights = [tf.cast(var, dtype=dtype)
+                      for var in self.vars]
       normalized_weights = tf.nn.softmax(tf.stack(edge_weights))
       nodes = tf.stack(nodes, axis=-1)
       new_node = tf.reduce_sum(nodes * normalized_weights, -1)
     elif self.weight_method == 'fastattn':
-      edge_weights = []
-      for var in self.vars:
-        var = tf.cast(var, dtype=dtype)
-        edge_weights.append(var)
+      edge_weights = [tf.nn.relu(tf.cast(var, dtype=dtype))
+                      for var in self.vars]
       weights_sum = add_n(edge_weights)
       nodes = [
           nodes[i] * edge_weights[i] / (weights_sum + 0.0001)
@@ -106,19 +102,14 @@ class FNode(tf.keras.layers.Layer):
       ]
       new_node = add_n(nodes)
     elif self.weight_method == 'channel_attn':
-      edge_weights = []
-      for var in self.vars:
-        var = tf.cast(var, dtype=dtype)
-        edge_weights.append(var)
+      edge_weights = [tf.cast(var, dtype=dtype)
+                      for var in self.vars]
       normalized_weights = tf.nn.softmax(tf.stack(edge_weights, -1), axis=-1)
       nodes = tf.stack(nodes, axis=-1)
       new_node = tf.reduce_sum(nodes * normalized_weights, -1)
     elif self.weight_method == 'channel_fastattn':
-      edge_weights = []
-      for var in self.vars:
-        var = tf.cast(var, dtype=dtype)
-        edge_weights.append(var)
-
+      edge_weights = [tf.nn.relu(tf.cast(var, dtype=dtype))
+                      for var in self.vars]
       weights_sum = add_n(edge_weights)
       nodes = [
           nodes[i] * edge_weights[i] / (weights_sum + 0.0001)
