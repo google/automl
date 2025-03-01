cbb80a58d209596be44b1a7b00832631406ac534
Chen Liang <crazydonkey200@gmail.com>
2025-03-01 00:04:05 -0800

    Update local_test.sh
    Add more description.

--- hero/local_test.sh
+++ hero/local_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Example command for local run.
-export JAX_DISABLE_JIT=True; EXP=local_test_1; rm -rf /tmp/${EXP}; python main.py --experiment_config TransformerLMTest --experiment_dir /tmp/${EXP} --verbosity=-1
+# Example command for local run. 
+# Add "export JAX_DISABLE_JIT=True;" to disable `jit` for easier debugging.
+# Change "TransformerLMTest" to other experiment config names in `config_lib.py` to run other experiments.
+EXP=local_test_1; rm -rf /tmp/${EXP}; python main.py --experiment_config TransformerLMTest --experiment_dir /tmp/${EXP} --verbosity=-1
 
 # Check training curves.
 tensorboard --logdir /tmp/${EXP}
